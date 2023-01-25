2de2d85d1f50d00817422eaf81f3b4c0c8833b84
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-01-25 17:44:53 +0530

    kyc api done

--- app/src/main/java/com/example/coinclubapp/InterFace/ApiInterface.java
+++ app/src/main/java/com/example/coinclubapp/InterFace/ApiInterface.java
@@ -1,5 +1,8 @@
 package com.example.coinclubapp.InterFace;
 
+import android.view.ViewDebug;
+
+import com.example.coinclubapp.Response.KycResponse;
 import com.example.coinclubapp.Response.Response;
 
 import retrofit2.Call;
@@ -21,4 +24,20 @@ public interface ApiInterface {
                              @Field("deposite") String deposite,
                              @Field("motive") String motive,
                              @Field("income") String income);
+
+    @FormUrlEncoded
+    @POST("user-update")
+    Call<KycResponse> PostKycItems(
+            @Field("id") String id,
+            @Field("full_name") String full_name,
+            @Field("kyc_mobile") String kyc_mobile,
+            @Field("address") String address,
+            @Field("id_name") String id_name,
+            @Field("id_number") String id_number,
+            @Field("email_id") String email_id,
+            @Field("id_frontimg") String id_frontimg,
+            @Field("id_back_img") String id_back_img,
+            @Field("driving_licence_no") String driving_licence_no,
+            @Field("licence_expiry_date") String licence_expiry_date
+    );
 }
