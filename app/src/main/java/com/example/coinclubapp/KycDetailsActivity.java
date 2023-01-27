58d1b0730cf6530c3d64b21b4bb9430f1f656687
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-01-27 10:55:19 +0530

    kyc api done..

--- app/src/main/java/com/example/coinclubapp/KycDetailsActivity.java
+++ app/src/main/java/com/example/coinclubapp/KycDetailsActivity.java
@@ -108,6 +108,7 @@ public class KycDetailsActivity extends AppCompatActivity {
                             Toast.makeText(KycDetailsActivity.this, "Kyc Successful", Toast.LENGTH_SHORT).show();
                             finish();
                         } else {
+
                             Toast.makeText(KycDetailsActivity.this, response.message(), Toast.LENGTH_SHORT).show();
                         }
                     }
