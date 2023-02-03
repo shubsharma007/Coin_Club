44c5e2ed7907800bfa65873dc0768832084efd59
Shubham <agehlot806@gmail.com>
2023-02-03 17:57:26 +0530

    login api

--- app/src/main/java/com/example/coinclubapp/ClubJoiningFormOneActivity.java
+++ app/src/main/java/com/example/coinclubapp/ClubJoiningFormOneActivity.java
@@ -19,16 +19,15 @@ public class ClubJoiningFormOneActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityClubJoiningFormOneBinding.inflate (getLayoutInflater ());
-        setContentView(binding.getRoot ());
-        String mobile=getIntent().getStringExtra("mobile");
-        binding.mobileNoEt.setText(mobile);
+        binding = ActivityClubJoiningFormOneBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // String mobile=getIntent().getStringExtra("mobile");
+        // binding.mobileNoEt.setText(mobile);
 
         binding.rbSalary.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked)
-                {
+                if (isChecked) {
                     binding.rbStudent.setChecked(false);
                     binding.rbSelfEmployed.setChecked(false);
                     binding.rbHousewife.setChecked(false);
@@ -38,8 +37,7 @@ public class ClubJoiningFormOneActivity extends AppCompatActivity {
         binding.rbStudent.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                {
+                if (isChecked) {
                     binding.rbSalary.setChecked(false);
                     binding.rbSelfEmployed.setChecked(false);
                     binding.rbHousewife.setChecked(false);
@@ -60,8 +58,7 @@ public class ClubJoiningFormOneActivity extends AppCompatActivity {
         binding.rbHousewife.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                {
+                if (isChecked) {
                     binding.rbStudent.setChecked(false);
                     binding.rbSelfEmployed.setChecked(false);
                     binding.rbSalary.setChecked(false);
@@ -69,92 +66,84 @@ public class ClubJoiningFormOneActivity extends AppCompatActivity {
             }
         });
 
-        String name =  binding.fullNameEt.getText ().toString ().trim ();
-        String city = binding.cityEt.getText ().toString ().trim ();
-        String organisation = binding.noyoEt.getText ().toString ().trim ();
+        String name = binding.fullNameEt.getText().toString().trim();
+        String city = binding.cityEt.getText().toString().trim();
+        String organisation = binding.noyoEt.getText().toString().trim();
 
         binding.nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fullName=binding.fullNameEt.getText().toString().trim();
+                String fullName = binding.fullNameEt.getText().toString().trim();
                 String[] arrOfStr = fullName.split(" ");
-                if(binding.fullNameEt.getText().toString().isEmpty())
-                {
+                if (binding.fullNameEt.getText().toString().isEmpty()) {
                     binding.fullNameEt.setError("enter your name");
                     binding.fullNameEt.requestFocus();
-                }
-                else if(arrOfStr.length<2)
-                {
+                } else if (arrOfStr.length < 2) {
                     binding.fullNameEt.setError("enter your full name");
                     binding.fullNameEt.requestFocus();
-                }
-//                else if(binding.mobileNoEt.getText().toString().trim().length()!=10){
-//                    binding.mobileNoEt.setError("enter correct mobile no");
-//                    binding.mobileNoEt.requestFocus();
-//                }
-                else if(binding.cityEt.getText().toString().trim().isEmpty())
-                {
+                } else if (binding.mobileNoEt.getText().toString().trim().length() != 10) {
+                    binding.mobileNoEt.setError("enter correct mobile no");
+                    binding.mobileNoEt.requestFocus();
+                } else if (binding.cityEt.getText().toString().trim().isEmpty()) {
                     binding.cityEt.setError("enter city name");
                     binding.cityEt.requestFocus();
-                }
-                else if(!binding.rbMale.isChecked() && !binding.rbFemale.isChecked())
-                {
+                } else if (binding.etPassword.getText().toString().trim().isEmpty()) {
+                    binding.etPassword.setError("enter phone number");
+                    binding.etPassword.requestFocus();
+                } else if (!binding.rbMale.isChecked() && !binding.rbFemale.isChecked()) {
                     Toast.makeText(ClubJoiningFormOneActivity.this, "enter your gender", Toast.LENGTH_SHORT).show();
-                }
-                else if(!binding.rbSalary.isChecked() && !binding.rbHousewife.isChecked() && !binding.rbStudent.isChecked() && !binding.rbSelfEmployed.isChecked())
-                {
+                } else if (!binding.rbSalary.isChecked() && !binding.rbHousewife.isChecked() && !binding.rbStudent.isChecked() && !binding.rbSelfEmployed.isChecked()) {
                     Toast.makeText(ClubJoiningFormOneActivity.this, "please enter your occupation", Toast.LENGTH_SHORT).show();
-                }
-                else if(binding.noyoEt.getText().toString().isEmpty())
-                {
+                } else if (binding.noyoEt.getText().toString().isEmpty()) {
                     binding.noyoEt.setError("enter organisation name");
                     binding.noyoEt.requestFocus();
-                }
-                else
-                {
+                } else {
                     passIntent = new Intent(ClubJoiningFormOneActivity.this, ClubJoiningFormTwoActivity.class);
-                    String name=binding.fullNameEt.getText().toString();
-                    passIntent.putExtra("full_name",name);
-                    Log.i("USER_ENTERED",name);
+                    String name = binding.fullNameEt.getText().toString();
+                    passIntent.putExtra("full_name", name);
+                    Log.i("USER_ENTERED", name);
 
-                    String city=binding.cityEt.getText().toString();
+                    String city = binding.cityEt.getText().toString();
                     passIntent.putExtra("city", city);
-                    Log.i("USER_ENTERED",city);
+                    Log.i("USER_ENTERED", city);
 
                     if (binding.rbMale.isChecked()) {
                         passIntent.putExtra("gender", "male");
-                        Log.i("USER_ENTERED","male");
+                        Log.i("USER_ENTERED", "male");
                     } else {
                         passIntent.putExtra("gender", "female");
-                        Log.i("USER_ENTERED","female");
+                        Log.i("USER_ENTERED", "female");
                     }
 
 
                     if (binding.rbSalary.isChecked()) {
                         passIntent.putExtra("occupation", "salaried professional");
-                        Log.i("USER_ENTERED","salaried");
+                        Log.i("USER_ENTERED", "salaried");
                     } else if (binding.rbStudent.isChecked()) {
                         passIntent.putExtra("occupation", "student");
-                        Log.i("USER_ENTERED","student");
+                        Log.i("USER_ENTERED", "student");
                     } else if (binding.rbSelfEmployed.isChecked()) {
                         passIntent.putExtra("occupation", "self employed");
-                        Log.i("USER_ENTERED","self");
+                        Log.i("USER_ENTERED", "self");
                     } else {
                         passIntent.putExtra("occupation", "house wife");
-                        Log.i("USER_ENTERED","wife");
+                        Log.i("USER_ENTERED", "wife");
                     }
 
-                    String noyo=binding.noyoEt.getText().toString();
+                    String noyo = binding.noyoEt.getText().toString();
                     passIntent.putExtra("organization", noyo);
-                    Log.i("USER_ENTERED",noyo);
+                    Log.i("USER_ENTERED", noyo);
 
-                    String mobile=binding.mobileNoEt.getText().toString();
-                    passIntent.putExtra("mobile",mobile);
-                    Log.i("USER_ENTERED",mobile);
+                    String mobile = binding.mobileNoEt.getText().toString();
+                    passIntent.putExtra("mobile", mobile);
+                    Log.i("USER_ENTERED", mobile);
 
-                    String pw=getIntent().getStringExtra("password");
-                    passIntent.putExtra("password",pw);
-                    Log.i("USER_ENTERED",pw);
+                    String password = binding.etPassword.getText().toString();
+                    passIntent.putExtra("password",password);
+                    Log.i("USER_ENTERED",password);
+//                    String pw = getIntent().getStringExtra("password");
+//                    passIntent.putExtra("password", pw);
+//                    Log.i("USER_ENTERED", pw);
 
 
                     startActivity(passIntent);
--- app/src/main/java/com/example/coinclubapp/LoginActivity.java
+++ app/src/main/java/com/example/coinclubapp/LoginActivity.java
@@ -1,44 +1,99 @@
 package com.example.coinclubapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.coinclubapp.InterFace.ApiInterface;
+import com.example.coinclubapp.Response.LoginResponse;
+import com.example.coinclubapp.Retrofit.RetrofitService;
 import com.example.coinclubapp.databinding.ActivityLoginBinding;
+import com.example.coinclubapp.result.LoginResult;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
     ActivityLoginBinding binding;
+    Dialog adDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         binding.goBtn.setOnClickListener(v -> {
-            if(binding.edMobile.getText ().toString ().trim ().isEmpty ()) {
-                binding.edMobile.setError ("Please Enter Mobile Number");
-                binding.edMobile.requestFocus ();
-            }else if(binding.edMobile.getText ().toString ().trim ().length () < 10){
-                binding.edMobile.setError ("Enter Correct Mobile Number");
-                binding.edMobile.requestFocus ();
-            } else if(binding.edPassword.getText ().toString ().trim ().isEmpty ()){
-                binding.edPassword.setError ("Enter Password Number");
-                binding.edPassword.requestFocus ();
+            if (binding.edMobile.getText().toString().trim().isEmpty()) {
+                binding.edMobile.setError("Please Enter Mobile Number");
+                binding.edMobile.requestFocus();
+            } else if (binding.edMobile.getText().toString().trim().length() < 10) {
+                binding.edMobile.setError("Enter Correct Mobile Number");
+                binding.edMobile.requestFocus();
+            } else if (binding.edPassword.getText().toString().trim().isEmpty()) {
+                binding.edPassword.setError("Enter Password Number");
+                binding.edPassword.requestFocus();
             } else {
-                Intent i = new Intent (LoginActivity.this, ClubJoiningFormOneActivity.class);
-                String mobile=binding.edMobile.getText().toString();
-                String pw=binding.edPassword.getText().toString();
-                Log.i("USER_ENTERED",mobile);
-                Log.i("USER_ENTERED",pw);
-                i.putExtra("mobile",mobile);
-                i.putExtra("password",pw);
-                startActivity (i);
+
+                String mobile = binding.edMobile.getText().toString();
+                String password = binding.edPassword.getText().toString();
+                ApiInterface apiInterface = RetrofitService.getRetrofit().create(ApiInterface.class);
+                Call<LoginResponse> call = apiInterface.UserLogin(mobile, password);
+                call.enqueue(new Callback<LoginResponse>() {
+                    @Override
+                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                        if (response.isSuccessful()) {
+                            LoginResponse loginResponse = response.body();
+
+                            if (loginResponse.getStatus()) {
+
+                               // LoginResult login_result = loginResponse.getLogin_data();
+
+                                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                                    Toast.makeText(LoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                                    startActivity(i);
+
+                            } else {
+
+                                Toast.makeText(LoginActivity.this, "incorrect phone or password...", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResponse> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
             }
+        });
+        binding.btnSignUp.setOnClickListener(v -> {
+
+            Intent i = new Intent(LoginActivity.this, ClubJoiningFormOneActivity.class);
+//                String mobile=binding.edMobile.getText().toString();
+//                String pw=binding.edPassword.getText().toString();
+//                Log.i("USER_ENTERED",mobile);
+//                Log.i("USER_ENTERED",pw);
+//                i.putExtra("mobile",mobile);
+//                i.putExtra("password",pw);
+            startActivity(i);
+
         });
     }
 }
