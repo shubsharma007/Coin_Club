92fed9dec6e15707b912319acc0978184c99aab3
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-02-08 10:26:16 +0530

    api done

--- app/src/main/java/com/example/coinclubapp/result/KycResult.java
+++ app/src/main/java/com/example/coinclubapp/result/KycResult.java
@@ -1,35 +1,17 @@
 package com.example.coinclubapp.result;
 
 public class KycResult {
-    private int id;
-    private String type;
-    private String full_name;
-    private String city;
-    private String gender;
-    private String occupation;
-    private String organization;
-    private String deposite;
-    private String motive;
-    private String income;
-    private String mobile;
-    private String address;
-    private String email;
-    private String kyc_mobile;
-    private String id_name;
-    private String id_number;
-    private String id_frontimg;
-    private String id_back_img;
-    private String driving_licence_no;
-    private String livence_expiry_date;
-    private Object profile_img;
-    private Object lng;
-    private Object notification;
-    private String device_type;
-    private Object device_token;
-    private Object api_token;
-    private String created_at;
-    private String updated_at;
 
+    private int id;
+    private String full_name;
+    private String mobileno;
+    private String aadharno;
+    private String aadharfrontimg;
+    private String aadharbackimg;
+    private String panno;
+    private String panfrontimg;
+    private String panbackimg;
+    private boolean is_verified;
 
     public int getId() {
         return id;
@@ -37,14 +19,6 @@ public class KycResult {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public String getFull_name() {
@@ -55,203 +29,67 @@ public class KycResult {
         this.full_name = full_name;
     }
 
-    public String getCity() {
-        return city;
+    public String getMobileno() {
+        return mobileno;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setMobileno(String mobileno) {
+        this.mobileno = mobileno;
     }
 
-    public String getGender() {
-        return gender;
+    public String getAadharno() {
+        return aadharno;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
+    public void setAadharno(String aadharno) {
+        this.aadharno = aadharno;
     }
 
-    public String getOccupation() {
-        return occupation;
+    public String getAadharfrontimg() {
+        return aadharfrontimg;
     }
 
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
+    public void setAadharfrontimg(String aadharfrontimg) {
+        this.aadharfrontimg = aadharfrontimg;
     }
 
-    public String getOrganization() {
-        return organization;
+    public String getAadharbackimg() {
+        return aadharbackimg;
     }
 
-    public void setOrganization(String organization) {
-        this.organization = organization;
+    public void setAadharbackimg(String aadharbackimg) {
+        this.aadharbackimg = aadharbackimg;
     }
 
-    public String getDeposite() {
-        return deposite;
+    public String getPanno() {
+        return panno;
     }
 
-    public void setDeposite(String deposite) {
-        this.deposite = deposite;
+    public void setPanno(String panno) {
+        this.panno = panno;
     }
 
-    public String getMotive() {
-        return motive;
+    public String getPanfrontimg() {
+        return panfrontimg;
     }
 
-    public void setMotive(String motive) {
-        this.motive = motive;
+    public void setPanfrontimg(String panfrontimg) {
+        this.panfrontimg = panfrontimg;
     }
 
-    public String getIncome() {
-        return income;
+    public String getPanbackimg() {
+        return panbackimg;
     }
 
-    public void setIncome(String income) {
-        this.income = income;
+    public void setPanbackimg(String panbackimg) {
+        this.panbackimg = panbackimg;
     }
 
-    public String getMobile() {
-        return mobile;
+    public boolean isIs_verified() {
+        return is_verified;
     }
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getKyc_mobile() {
-        return kyc_mobile;
-    }
-
-    public void setKyc_mobile(String kyc_mobile) {
-        this.kyc_mobile = kyc_mobile;
-    }
-
-    public String getId_name() {
-        return id_name;
-    }
-
-    public void setId_name(String id_name) {
-        this.id_name = id_name;
-    }
-
-    public String getId_number() {
-        return id_number;
-    }
-
-    public void setId_number(String id_number) {
-        this.id_number = id_number;
-    }
-
-    public String getId_frontimg() {
-        return id_frontimg;
-    }
-
-    public void setId_frontimg(String id_frontimg) {
-        this.id_frontimg = id_frontimg;
-    }
-
-    public String getId_back_img() {
-        return id_back_img;
-    }
-
-    public void setId_back_img(String id_back_img) {
-        this.id_back_img = id_back_img;
-    }
-
-    public String getDriving_licence_no() {
-        return driving_licence_no;
-    }
-
-    public void setDriving_licence_no(String driving_licence_no) {
-        this.driving_licence_no = driving_licence_no;
-    }
-
-    public String getLivence_expiry_date() {
-        return livence_expiry_date;
-    }
-
-    public void setLivence_expiry_date(String livence_expiry_date) {
-        this.livence_expiry_date = livence_expiry_date;
-    }
-
-    public Object getProfile_img() {
-        return profile_img;
-    }
-
-    public void setProfile_img(Object profile_img) {
-        this.profile_img = profile_img;
-    }
-
-    public Object getLng() {
-        return lng;
-    }
-
-    public void setLng(Object lng) {
-        this.lng = lng;
-    }
-
-    public Object getNotification() {
-        return notification;
-    }
-
-    public void setNotification(Object notification) {
-        this.notification = notification;
-    }
-
-    public String getDevice_type() {
-        return device_type;
-    }
-
-    public void setDevice_type(String device_type) {
-        this.device_type = device_type;
-    }
-
-    public Object getDevice_token() {
-        return device_token;
-    }
-
-    public void setDevice_token(Object device_token) {
-        this.device_token = device_token;
-    }
-
-    public Object getApi_token() {
-        return api_token;
-    }
-
-    public void setApi_token(Object api_token) {
-        this.api_token = api_token;
-    }
-
-    public String getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(String created_at) {
-        this.created_at = created_at;
-    }
-
-    public String getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(String updated_at) {
-        this.updated_at = updated_at;
+    public void setIs_verified(boolean is_verified) {
+        this.is_verified = is_verified;
     }
 }
