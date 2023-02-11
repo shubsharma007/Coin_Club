a3f1190476e4f41715a79cfd08a78bb3cbb8dc88
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-02-11 12:44:15 +0530

    apis

--- app/src/main/java/com/example/coinclubapp/BidRoomActivity.java
+++ app/src/main/java/com/example/coinclubapp/BidRoomActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.coinclubapp.Fragments.BidNowFragment;
+import com.example.coinclubapp.databinding.ActivityBidRoomBinding;
+
 public class BidRoomActivity extends AppCompatActivity {
+    ActivityBidRoomBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bid_room);
+        binding = ActivityBidRoomBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bidBtn.setOnClickListener(v -> {
+
+            BidNowFragment bidNowFragment =new BidNowFragment();
+            bidNowFragment.show(getSupportFragmentManager(),bidNowFragment.getTag());
+
+        });
+
     }
 }
